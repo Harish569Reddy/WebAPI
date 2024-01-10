90bf008d114544c811620639c2a7c1aaf2d53927
khalid Ansari <khalid.ansari@CTPL.in>
2024-01-10 18:16:01 +0530

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            // Replace this with your actual data retrieval logic
+            List<string> items = new List<string>
+        {
+            "Phone",
+            "Camera",
+            "Mobile"
+        };
+
+            return items;
+        }
+    }
+}
